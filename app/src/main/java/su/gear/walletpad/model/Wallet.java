7faddfc207dff475a50066c2a5d78609c548128e
Egor Makarenko <egormkn@yandex.ru>
2016-12-15 18:15:50 +0300

    Wallet model

--- app/src/main/java/su/gear/walletpad/model/Wallet.java
+++ app/src/main/java/su/gear/walletpad/model/Wallet.java
@@ -4,6 +4,36 @@ package su.gear.walletpad.model;
  * Объект кошелька
  */
 
-public class Wallet {
+public class Wallet implements WalletsListItem {
 
+    public enum Type {
+        CARD, CASH, DEPOSIT, STOCK
+    };
+
+    private final long id;
+
+    private final String title;
+
+    private final double amount;
+
+    private final Type type;
+
+    public Wallet(double amount, long id, String title, Type type) {
+        this.amount = amount;
+        this.id = id;
+        this.title = title;
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Type getType() {
+        return type;
+    }
 }
